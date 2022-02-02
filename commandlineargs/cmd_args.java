8c7061d38539fef2a06afb58bad9b54f84970c7e
Subin <subinsk284@gmail.com>
2022-02-02 12:53:49 +0530

    command line args

---
+++ commandlineargs/cmd_args.java
@@ -0,0 +1,9 @@
+package commandlineargs;
+
+public class cmd_args {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
