374ca761baed826f5afa351253178c5dc93f6d61
Subin <subinsk284@gmail.com>
2022-02-05 10:20:43 +0530

    byte data type

---
+++ bytedatatype/bytedatatype.java
@@ -0,0 +1,15 @@
+package bytedatatype;
+
+public class bytedatatype {
+    public static void main(String[] args) {
+        byte a = 11, b = 10;
+        byte add = (byte) (a + b);
+        byte sub = (byte) (a - b);
+        byte multi = (byte) (a * b);
+        byte divide = (byte) (a / b);
+        System.out.println("\nAddition: " + add);
+        System.out.println("\nSubtraction: " + sub);
+        System.out.println("\nMultiplication: " + multi);
+        System.out.println("\nDivision: " + divide);
+    }
+}
